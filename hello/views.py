177a2a93b9826acbfeb960b753cf74996a24f78d
nxbfuentes <nxbfuentes@gmail.com>
2023-06-02 13:18:08 +0800

    added file adding features, although with no file checking or storing, only to check if there is file or not

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def hello_view(request):
     if request.method == 'POST':
         text = request.POST.get('text')
-        return HttpResponse(f"Creating a quiz based on:\n {text}")
+        file = request.FILES.get('file')
+        if file is not None:
+            # Do something with the file
+            pass
+        return HttpResponse(f"Creating a quiz based on: {text}")
     else:
         return render(request, 'hello.html')
